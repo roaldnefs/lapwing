3b801c3933309eb1617aebc29e90b7b1d8445435
Roald Nefs <info@roaldnefs.com>
2020-05-13 21:28:25 +0200

    feat: add initial input file descriptor buffer
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

---
+++ include/lapwing/ifdbuf.h
@@ -0,0 +1,114 @@
+#ifndef INCLUDED_LAPWING_IFDBUF_
+#define INCLUDED_LAPWING_IFDBUF_
+
+#include <streambuf>
+#include <unistd.h>
+
+namespace lapwing {
+
+// IFdBuf reads from a file descriptor device using an input buffer.
+//
+// Example:
+//    IFdBuf fds(STDIN_FILENO, 30);
+//    char buf[80];
+//    fds.sgetn(buf, 80);  // reads block of 80 chars
+class IFdBuf: public std::streambuf {
+    // Holds the buffer size.
+    size_t d_bufsize;
+    // Holds the file descriptor.
+    int d_fd;
+    // Holds the buffer itself.
+    char *d_buffer;
+
+    public:
+        IFdBuf();
+        IFdBuf(int fd, size_t bufsize = 1);
+
+        virtual ~IFdBuf();
+    
+        void open(int fd, size_t bufsize = 1);
+    private:
+        virtual int underflow();
+        virtual std::streamsize xsgetn(char *dest, std::streamsize n);
+        void cleanup();
+};
+
+// The default contstructor merely initializes the buffer to 0.
+inline IFdBuf::IFdBuf()
+    : d_bufsize(0),
+      d_fd(0) {}
+
+// Constructor parsing the file descriptions and buffer size to the open member
+// function.
+inline IFdBuf::IFdBuf(int fd, size_t bufsize) {
+    open(fd, bufsize);
+}
+
+// Destructor, calls cleanup.
+inline IFdBuf::~IFdBuf() {
+    cleanup();
+}
+
+// Cleanup deletes the old buffer and closes the file descriptor.
+inline void IFdBuf::cleanup() {
+    if (d_buffer)
+        delete[] d_buffer;
+
+    // TODO allow the file descriptor to stay open for re-use.
+    if (d_fd != -1)
+        close(d_fd);
+}
+
+// Open initializes the buffer. After allocating the buffer streambuf::setg is
+// called.
+inline void IFdBuf::open(int fd, size_t bufsize) {
+    d_fd = fd;
+    d_bufsize = bufsize;
+
+    cleanup();
+    d_buffer = new char[d_bufsize];
+    setg(d_buffer, d_buffer + d_bufsize, d_buffer + d_bufsize);
+}
+
+// Underflow fills the buffer by reading from the file descriptor. It returns
+// EOF if it fails.
+inline int IFdBuf::underflow() {
+    if (gptr() < egptr())
+        return *gptr();
+
+    int nread = read(d_fd, d_buffer, d_bufsize);
+
+    if (nread <= 0)
+        return EOF;
+
+    setg(d_buffer, d_buffer, d_buffer + nread);
+    return static_cast<unsigned char>(*gptr());
+}
+
+// xsgetn gets sequence of characters.
+inline std::streamsize IFdBuf::xsgetn(char *dest, std::streamsize n) {
+    int nread = 0;
+
+    while (n) {
+        if (!in_avail()) {
+            if (underflow() == EOF)
+                break;
+        }
+
+        int avail = in_avail();
+
+        if (avail > n)
+            avail = n;
+
+        memcpy(dest + nread, gptr(), avail);
+        gbump(avail);
+
+        nread += avail;
+        n -= avail;
+    }
+    return nread;
+}
+
+} // namespace lapwing
+
+#endif
